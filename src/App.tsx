6ba580ffd56070e299b66fea7abd13b32717143f
Karel Hala <ch4rlien@gmail.com>
2024-01-18 12:24:34 +0100

    Revert "Redirect to user access if user is not org admin"

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,21 @@ import { notificationsReducer } from '@redhat-cloud-services/frontend-components
 
 import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Reducer } from 'redux';
 import './App.scss';
 
 import { Routes } from './Routes';
-import { Bullseye, Spinner } from '@patternfly/react-core';
-import { useNavigate } from 'react-router-dom';
 
 const App = () => {
-  const [isOrgAdmin, setIsOrgAdmin] = useState<boolean | undefined>();
-  const { updateDocumentTitle, auth } = useChrome();
-  const navigate = useNavigate();
+  const { updateDocumentTitle } = useChrome();
 
   useEffect(() => {
     const registry = getRegistry();
     registry.register({ notifications: notificationsReducer as Reducer });
     // You can use directly the name of your app
     updateDocumentTitle('Service Accounts');
-    const getUser = () => auth.getUser();
-    getUser().then((data) => {
-      setIsOrgAdmin(data?.identity.user?.is_org_admin);
-    });
   }, []);
-
-  if (isOrgAdmin === undefined) {
-    return (
-      <Bullseye>
-        <Spinner />
-      </Bullseye>
-    );
-  }
-
-  if (!isOrgAdmin) {
-    navigate('/iam/my-user-access');
-  }
 
   return (
     <Fragment>
